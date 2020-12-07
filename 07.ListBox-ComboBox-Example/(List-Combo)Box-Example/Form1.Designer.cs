334927ddfd6ee9317ed87d9c42f9d69306ec2888
kagancansit <kagancansit@hotmail.com>
2020-12-07 16:26:33 +0300

    Students Example

---
+++ 07.ListBox-ComboBox-Example/(List-Combo)Box-Example/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace _List_Combo_Box_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeadLabel = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.GenderBox = new System.Windows.Forms.ComboBox();
+            this.StatusBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DetailBox = new System.Windows.Forms.ListBox();
+            this.AddBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HeadLabel
+            // 
+            this.HeadLabel.AutoSize = true;
+            this.HeadLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.HeadLabel.Location = new System.Drawing.Point(185, 42);
+            this.HeadLabel.Name = "HeadLabel";
+            this.HeadLabel.Size = new System.Drawing.Size(199, 25);
+            this.HeadLabel.TabIndex = 0;
+            this.HeadLabel.Text = "Öğrenci Kayıt Sistemi";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(101, 92);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(163, 22);
+            this.NameBox.TabIndex = 1;
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(101, 137);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(163, 22);
+            this.SurnameBox.TabIndex = 2;
+            // 
+            // GenderBox
+            // 
+            this.GenderBox.FormattingEnabled = true;
+            this.GenderBox.Items.AddRange(new object[] {
+            "Erkek",
+            "Kız"});
+            this.GenderBox.Location = new System.Drawing.Point(101, 184);
+            this.GenderBox.Name = "GenderBox";
+            this.GenderBox.Size = new System.Drawing.Size(163, 24);
+            this.GenderBox.TabIndex = 3;
+            // 
+            // StatusBox
+            // 
+            this.StatusBox.FormattingEnabled = true;
+            this.StatusBox.Items.AddRange(new object[] {
+            "Sabahçı",
+            "Öğlenci"});
+            this.StatusBox.Location = new System.Drawing.Point(101, 235);
+            this.StatusBox.Name = "StatusBox";
+            this.StatusBox.Size = new System.Drawing.Size(163, 24);
+            this.StatusBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cinsiyet:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 238);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Durum:";
+            // 
+            // DetailBox
+            // 
+            this.DetailBox.FormattingEnabled = true;
+            this.DetailBox.ItemHeight = 16;
+            this.DetailBox.Location = new System.Drawing.Point(291, 92);
+            this.DetailBox.Name = "DetailBox";
+            this.DetailBox.Size = new System.Drawing.Size(241, 164);
+            this.DetailBox.TabIndex = 9;
+            // 
+            // AddBttn
+            // 
+            this.AddBttn.Location = new System.Drawing.Point(124, 283);
+            this.AddBttn.Name = "AddBttn";
+            this.AddBttn.Size = new System.Drawing.Size(126, 30);
+            this.AddBttn.TabIndex = 10;
+            this.AddBttn.Text = "Ekle";
+            this.AddBttn.UseVisualStyleBackColor = true;
+            this.AddBttn.Click += new System.EventHandler(this.AddBttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 337);
+            this.Controls.Add(this.AddBttn);
+            this.Controls.Add(this.DetailBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StatusBox);
+            this.Controls.Add(this.GenderBox);
+            this.Controls.Add(this.SurnameBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.HeadLabel);
+            this.Name = "Form1";
+            this.Text = "Öğrenci Kayıt Sistemi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeadLabel;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox SurnameBox;
+        private System.Windows.Forms.ComboBox GenderBox;
+        private System.Windows.Forms.ComboBox StatusBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox DetailBox;
+        private System.Windows.Forms.Button AddBttn;
+    }
+}
+
---
+++ 07.ListBox-ComboBox-Example/(List-Combo)Box-Example/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _List_Combo_Box_Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            Uygulama10: Klavyeden adı ve soyadı girilen, ComboBox'tan cinsiyeti ve bir diğer ComboBox'tan
+            sabahçı mı yoksa öğlenci mi olduğu seçilen öğrencilerin bilgilerini ListBox'a yadıran uygulamayı yazınız.
+        */
+
+        //Butona basıldığında TextBox'lar ve ComboBox'larda bulanan verileri ListBox'a eklemesini istiyoruz.
+        private void AddBttn_Click(object sender, EventArgs e)
+        {
+            DetailBox.Items.Add(NameBox.Text + " " + SurnameBox.Text + " " + GenderBox.Text + " " + StatusBox.Text);
+        }
+    }
+}
