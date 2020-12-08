199db2e629b103d272c77cc39a0baa2e8dab4b34
kagancansit <kagancansit@hotmail.com>
2020-12-08 15:55:29 +0300

    Basic Double

---
+++ 20.Double/Double/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Double
+{
+    partial class DoubleExamples
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.DoubleLabel = new System.Windows.Forms.Label();
+            this.WriteBtn = new System.Windows.Forms.Button();
+            this.GatherButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.DoubleTextBox = new System.Windows.Forms.TextBox();
+            this.DoubleWriteButton = new System.Windows.Forms.Button();
+            this.DoubleWriteLabel = new System.Windows.Forms.Label();
+            this.MatBox1 = new System.Windows.Forms.TextBox();
+            this.MatBox2 = new System.Windows.Forms.TextBox();
+            this.MakeMathsButton = new System.Windows.Forms.Button();
+            this.ResultsLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.WriteBtn);
+            this.groupBox1.Controls.Add(this.DoubleLabel);
+            this.groupBox1.Location = new System.Drawing.Point(24, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 235);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uygulama16";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ResultLabel);
+            this.groupBox2.Controls.Add(this.GatherButton);
+            this.groupBox2.Location = new System.Drawing.Point(24, 263);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(327, 235);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Uygulama17";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.DoubleWriteLabel);
+            this.groupBox3.Controls.Add(this.DoubleWriteButton);
+            this.groupBox3.Controls.Add(this.DoubleTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(357, 22);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(327, 235);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Uygulama18";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.ResultsLabel);
+            this.groupBox4.Controls.Add(this.MakeMathsButton);
+            this.groupBox4.Controls.Add(this.MatBox2);
+            this.groupBox4.Controls.Add(this.MatBox1);
+            this.groupBox4.Location = new System.Drawing.Point(357, 263);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(327, 235);
+            this.groupBox4.TabIndex = 1;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Uygulama19";
+            // 
+            // DoubleLabel
+            // 
+            this.DoubleLabel.AutoSize = true;
+            this.DoubleLabel.Location = new System.Drawing.Point(84, 92);
+            this.DoubleLabel.Name = "DoubleLabel";
+            this.DoubleLabel.Size = new System.Drawing.Size(143, 17);
+            this.DoubleLabel.TabIndex = 0;
+            this.DoubleLabel.Text = "Ondalıklı Sayı Değeri:";
+            // 
+            // WriteBtn
+            // 
+            this.WriteBtn.Location = new System.Drawing.Point(100, 125);
+            this.WriteBtn.Name = "WriteBtn";
+            this.WriteBtn.Size = new System.Drawing.Size(100, 33);
+            this.WriteBtn.TabIndex = 1;
+            this.WriteBtn.Text = "Yazdır";
+            this.WriteBtn.UseVisualStyleBackColor = true;
+            this.WriteBtn.Click += new System.EventHandler(this.WriteBtn_Click);
+            // 
+            // GatherButton
+            // 
+            this.GatherButton.Location = new System.Drawing.Point(100, 96);
+            this.GatherButton.Name = "GatherButton";
+            this.GatherButton.Size = new System.Drawing.Size(100, 33);
+            this.GatherButton.TabIndex = 2;
+            this.GatherButton.Text = "Topla";
+            this.GatherButton.UseVisualStyleBackColor = true;
+            this.GatherButton.Click += new System.EventHandler(this.GatherButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(84, 65);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(111, 17);
+            this.ResultLabel.TabIndex = 3;
+            this.ResultLabel.Text = "Toplam Sonucu:";
+            // 
+            // DoubleTextBox
+            // 
+            this.DoubleTextBox.Location = new System.Drawing.Point(82, 51);
+            this.DoubleTextBox.Name = "DoubleTextBox";
+            this.DoubleTextBox.Size = new System.Drawing.Size(167, 22);
+            this.DoubleTextBox.TabIndex = 0;
+            // 
+            // DoubleWriteButton
+            // 
+            this.DoubleWriteButton.Location = new System.Drawing.Point(117, 83);
+            this.DoubleWriteButton.Name = "DoubleWriteButton";
+            this.DoubleWriteButton.Size = new System.Drawing.Size(93, 34);
+            this.DoubleWriteButton.TabIndex = 1;
+            this.DoubleWriteButton.Text = "Değeri Al";
+            this.DoubleWriteButton.UseVisualStyleBackColor = true;
+            this.DoubleWriteButton.Click += new System.EventHandler(this.DoubleWriteButton_Click);
+            // 
+            // DoubleWriteLabel
+            // 
+            this.DoubleWriteLabel.AutoSize = true;
+            this.DoubleWriteLabel.Location = new System.Drawing.Point(82, 140);
+            this.DoubleWriteLabel.Name = "DoubleWriteLabel";
+            this.DoubleWriteLabel.Size = new System.Drawing.Size(143, 17);
+            this.DoubleWriteLabel.TabIndex = 2;
+            this.DoubleWriteLabel.Text = "Ondalıklı Sayı Değeri:";
+            // 
+            // MatBox1
+            // 
+            this.MatBox1.Location = new System.Drawing.Point(82, 22);
+            this.MatBox1.Name = "MatBox1";
+            this.MatBox1.Size = new System.Drawing.Size(167, 22);
+            this.MatBox1.TabIndex = 0;
+            // 
+            // MatBox2
+            // 
+            this.MatBox2.Location = new System.Drawing.Point(82, 50);
+            this.MatBox2.Name = "MatBox2";
+            this.MatBox2.Size = new System.Drawing.Size(167, 22);
+            this.MatBox2.TabIndex = 1;
+            // 
+            // MakeMathsButton
+            // 
+            this.MakeMathsButton.Location = new System.Drawing.Point(82, 78);
+            this.MakeMathsButton.Name = "MakeMathsButton";
+            this.MakeMathsButton.Size = new System.Drawing.Size(167, 34);
+            this.MakeMathsButton.TabIndex = 3;
+            this.MakeMathsButton.Text = "İşlemleri Gerçekleştir";
+            this.MakeMathsButton.UseVisualStyleBackColor = true;
+            this.MakeMathsButton.Click += new System.EventHandler(this.MakeMathsButton_Click);
+            // 
+            // ResultsLabel
+            // 
+            this.ResultsLabel.AutoSize = true;
+            this.ResultsLabel.Location = new System.Drawing.Point(79, 124);
+            this.ResultsLabel.Name = "ResultsLabel";
+            this.ResultsLabel.Size = new System.Drawing.Size(68, 17);
+            this.ResultsLabel.TabIndex = 4;
+            this.ResultsLabel.Text = "Sonuçlar:";
+            // 
+            // DoubleExamples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 528);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DoubleExamples";
+            this.Text = "Double Örnekleri";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button WriteBtn;
+        private System.Windows.Forms.Label DoubleLabel;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button GatherButton;
+        private System.Windows.Forms.Label DoubleWriteLabel;
+        private System.Windows.Forms.Button DoubleWriteButton;
+        private System.Windows.Forms.TextBox DoubleTextBox;
+        private System.Windows.Forms.Label ResultsLabel;
+        private System.Windows.Forms.Button MakeMathsButton;
+        private System.Windows.Forms.TextBox MatBox2;
+        private System.Windows.Forms.TextBox MatBox1;
+    }
+}
+
---
+++ 20.Double/Double/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Double
+{
+    public partial class DoubleExamples : Form
+    {
+        public DoubleExamples()
+        {
+            InitializeComponent();
+        }
+
+        //Uygulama16:Kod kısmından girilen ondalıklı sayıyı labela yazdıran programı yazınız.
+        private void WriteBtn_Click(object sender, EventArgs e)
+        {
+            double sayi = 2.94;
+            DoubleLabel.Text ="Ondalıklı Sayı Değeri: "+ sayi.ToString();
+        }
+
+
+        //Uygulama17:Kod kısmından girilen iki sayıyı toplayıp sonucu hesaplayan prgogramı yazınız.
+        private void GatherButton_Click(object sender, EventArgs e)
+        {
+            double value = 2.48, value1 = 3.57, gather = 0;
+            gather = value + value1;
+            ResultLabel.Text ="Toplam Sonucu: " + gather.ToString();
+        }
+
+        //Önemli! Klavyede veri alırken ToInt olarak değil ToDouble olarak veriyi çekmelisin.!!!
+        //Uygulama18:Klavyeden girilen ondalıklı bir sayıyı label'a yazdıran programı kodlayınız.
+        private void DoubleWriteButton_Click(object sender, EventArgs e)
+        {
+            double deger = 0;
+            deger = Convert.ToDouble(DoubleTextBox.Text);
+            DoubleWriteLabel.Text ="Ondalıklı Sayı Değeri: " + deger.ToString();
+        }
+
+        //Klavyeden girilen ondalıklı iki sayı için aritmetik 4 işlem yapan programı kodlayalım.
+        private void MakeMathsButton_Click(object sender, EventArgs e)
+        {
+            double math1 = 0, math2 = 0, Total = 0, Extraction = 0, Product = 0, Episode = 0;
+            math1 = Convert.ToDouble(MatBox1.Text);
+            math2 = Convert.ToDouble(MatBox2.Text);
+
+            Total = math1 + math2;
+            Extraction = math1 - math2;
+            Product = math1 * math2;
+            Episode = math1 / math2;
+
+            ResultsLabel.Text = "Sonuçlar:" + Environment.NewLine +
+                "Toplam Sonucu: " + Total.ToString() + Environment.NewLine +
+                "Çıkarma Sonucu: " + Extraction.ToString() + Environment.NewLine +
+                "Çarpma Sonucu: " + Product.ToString() + Environment.NewLine +
+                "Bölme Sonucu: " + Episode.ToString();
+        }
+    }
+}
