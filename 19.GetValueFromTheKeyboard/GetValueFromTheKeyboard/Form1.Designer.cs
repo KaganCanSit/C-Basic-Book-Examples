d8989e9c1acd1111b83bc58e03471170c7d3bfcc
kagancansit <kagancansit@hotmail.com>
2020-12-08 13:45:10 +0300

    FromTheKeyboard

---
+++ 19.GetValueFromTheKeyboard/GetValueFromTheKeyboard/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace GetValueFromTheKeyboard
+{
+    partial class BasicApps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ValueTextBox = new System.Windows.Forms.TextBox();
+            this.WriteButton = new System.Windows.Forms.Button();
+            this.WriteLabel = new System.Windows.Forms.Label();
+            this.CupeTextBox = new System.Windows.Forms.TextBox();
+            this.CupeButton = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.Value1TextBox = new System.Windows.Forms.TextBox();
+            this.Value2TextBox = new System.Windows.Forms.TextBox();
+            this.GatherButton = new System.Windows.Forms.Button();
+            this.GatherLabel = new System.Windows.Forms.Label();
+            this.EdgeBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SquareLabel = new System.Windows.Forms.Label();
+            this.ExamBox1 = new System.Windows.Forms.TextBox();
+            this.ExamBox2 = new System.Windows.Forms.TextBox();
+            this.AverageCalculateButton = new System.Windows.Forms.Button();
+            this.AverageCalculateLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ValueTextBox
+            // 
+            this.ValueTextBox.Location = new System.Drawing.Point(43, 41);
+            this.ValueTextBox.Name = "ValueTextBox";
+            this.ValueTextBox.Size = new System.Drawing.Size(217, 22);
+            this.ValueTextBox.TabIndex = 0;
+            // 
+            // WriteButton
+            // 
+            this.WriteButton.Location = new System.Drawing.Point(108, 69);
+            this.WriteButton.Name = "WriteButton";
+            this.WriteButton.Size = new System.Drawing.Size(102, 37);
+            this.WriteButton.TabIndex = 1;
+            this.WriteButton.Text = "Yazdır";
+            this.WriteButton.UseVisualStyleBackColor = true;
+            this.WriteButton.Click += new System.EventHandler(this.WriteButton_Click);
+            // 
+            // WriteLabel
+            // 
+            this.WriteLabel.AutoSize = true;
+            this.WriteLabel.Location = new System.Drawing.Point(40, 126);
+            this.WriteLabel.Name = "WriteLabel";
+            this.WriteLabel.Size = new System.Drawing.Size(85, 17);
+            this.WriteLabel.TabIndex = 2;
+            this.WriteLabel.Text = "Sayı Değeri:";
+            // 
+            // CupeTextBox
+            // 
+            this.CupeTextBox.Location = new System.Drawing.Point(43, 38);
+            this.CupeTextBox.Name = "CupeTextBox";
+            this.CupeTextBox.Size = new System.Drawing.Size(217, 22);
+            this.CupeTextBox.TabIndex = 5;
+            // 
+            // CupeButton
+            // 
+            this.CupeButton.Location = new System.Drawing.Point(98, 66);
+            this.CupeButton.Name = "CupeButton";
+            this.CupeButton.Size = new System.Drawing.Size(102, 37);
+            this.CupeButton.TabIndex = 6;
+            this.CupeButton.Text = "Kübünü Al";
+            this.CupeButton.UseVisualStyleBackColor = true;
+            this.CupeButton.Click += new System.EventHandler(this.CupeButton_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(40, 123);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(96, 17);
+            this.ResultLabel.TabIndex = 7;
+            this.ResultLabel.Text = "İşlem Sonucu:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ValueTextBox);
+            this.groupBox1.Controls.Add(this.WriteButton);
+            this.groupBox1.Controls.Add(this.WriteLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 155);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uygulama11";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.CupeTextBox);
+            this.groupBox2.Controls.Add(this.CupeButton);
+            this.groupBox2.Controls.Add(this.ResultLabel);
+            this.groupBox2.Location = new System.Drawing.Point(12, 185);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 155);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Uygulama12";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.GatherLabel);
+            this.groupBox3.Controls.Add(this.GatherButton);
+            this.groupBox3.Controls.Add(this.Value2TextBox);
+            this.groupBox3.Controls.Add(this.Value1TextBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 354);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(311, 155);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Uygulama13";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.AverageCalculateLabel);
+            this.groupBox5.Controls.Add(this.AverageCalculateButton);
+            this.groupBox5.Controls.Add(this.ExamBox2);
+            this.groupBox5.Controls.Add(this.ExamBox1);
+            this.groupBox5.Location = new System.Drawing.Point(346, 185);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(311, 155);
+            this.groupBox5.TabIndex = 12;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Uygulama15";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.SquareLabel);
+            this.groupBox6.Controls.Add(this.button2);
+            this.groupBox6.Controls.Add(this.EdgeBox);
+            this.groupBox6.Location = new System.Drawing.Point(346, 24);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(311, 155);
+            this.groupBox6.TabIndex = 11;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Uygulama14";
+            // 
+            // Value1TextBox
+            // 
+            this.Value1TextBox.Location = new System.Drawing.Point(43, 21);
+            this.Value1TextBox.Name = "Value1TextBox";
+            this.Value1TextBox.Size = new System.Drawing.Size(217, 22);
+            this.Value1TextBox.TabIndex = 0;
+            // 
+            // Value2TextBox
+            // 
+            this.Value2TextBox.Location = new System.Drawing.Point(43, 49);
+            this.Value2TextBox.Name = "Value2TextBox";
+            this.Value2TextBox.Size = new System.Drawing.Size(217, 22);
+            this.Value2TextBox.TabIndex = 1;
+            // 
+            // GatherButton
+            // 
+            this.GatherButton.Location = new System.Drawing.Point(98, 77);
+            this.GatherButton.Name = "GatherButton";
+            this.GatherButton.Size = new System.Drawing.Size(102, 32);
+            this.GatherButton.TabIndex = 2;
+            this.GatherButton.Text = "Topla";
+            this.GatherButton.UseVisualStyleBackColor = true;
+            this.GatherButton.Click += new System.EventHandler(this.GatherButton_Click);
+            // 
+            // GatherLabel
+            // 
+            this.GatherLabel.AutoSize = true;
+            this.GatherLabel.Location = new System.Drawing.Point(40, 121);
+            this.GatherLabel.Name = "GatherLabel";
+            this.GatherLabel.Size = new System.Drawing.Size(59, 17);
+            this.GatherLabel.TabIndex = 3;
+            this.GatherLabel.Text = "Toplam:";
+            // 
+            // EdgeBox
+            // 
+            this.EdgeBox.Location = new System.Drawing.Point(47, 41);
+            this.EdgeBox.Name = "EdgeBox";
+            this.EdgeBox.Size = new System.Drawing.Size(217, 22);
+            this.EdgeBox.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Alan Ve Çevre Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SquareLabel
+            // 
+            this.SquareLabel.AutoSize = true;
+            this.SquareLabel.Location = new System.Drawing.Point(44, 126);
+            this.SquareLabel.Name = "SquareLabel";
+            this.SquareLabel.Size = new System.Drawing.Size(213, 17);
+            this.SquareLabel.TabIndex = 2;
+            this.SquareLabel.Text = "Karenin Alanı: - Karenin Çevresi:";
+            // 
+            // ExamBox1
+            // 
+            this.ExamBox1.Location = new System.Drawing.Point(47, 21);
+            this.ExamBox1.Name = "ExamBox1";
+            this.ExamBox1.Size = new System.Drawing.Size(217, 22);
+            this.ExamBox1.TabIndex = 0;
+            // 
+            // ExamBox2
+            // 
+            this.ExamBox2.Location = new System.Drawing.Point(47, 50);
+            this.ExamBox2.Name = "ExamBox2";
+            this.ExamBox2.Size = new System.Drawing.Size(217, 22);
+            this.ExamBox2.TabIndex = 1;
+            // 
+            // AverageCalculateButton
+            // 
+            this.AverageCalculateButton.Location = new System.Drawing.Point(75, 78);
+            this.AverageCalculateButton.Name = "AverageCalculateButton";
+            this.AverageCalculateButton.Size = new System.Drawing.Size(171, 42);
+            this.AverageCalculateButton.TabIndex = 2;
+            this.AverageCalculateButton.Text = "Ortalama Hesapla";
+            this.AverageCalculateButton.UseVisualStyleBackColor = true;
+            this.AverageCalculateButton.Click += new System.EventHandler(this.AverageCalculateButton_Click);
+            // 
+            // AverageCalculateLabel
+            // 
+            this.AverageCalculateLabel.AutoSize = true;
+            this.AverageCalculateLabel.Location = new System.Drawing.Point(44, 123);
+            this.AverageCalculateLabel.Name = "AverageCalculateLabel";
+            this.AverageCalculateLabel.Size = new System.Drawing.Size(70, 17);
+            this.AverageCalculateLabel.TabIndex = 3;
+            this.AverageCalculateLabel.Text = "Ortalama:";
+            // 
+            // BasicApps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 521);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BasicApps";
+            this.Text = "Basit Uygulamalar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ValueTextBox;
+        private System.Windows.Forms.Button WriteButton;
+        private System.Windows.Forms.Label WriteLabel;
+        private System.Windows.Forms.TextBox CupeTextBox;
+        private System.Windows.Forms.Button CupeButton;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label GatherLabel;
+        private System.Windows.Forms.Button GatherButton;
+        private System.Windows.Forms.TextBox Value2TextBox;
+        private System.Windows.Forms.TextBox Value1TextBox;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label AverageCalculateLabel;
+        private System.Windows.Forms.Button AverageCalculateButton;
+        private System.Windows.Forms.TextBox ExamBox2;
+        private System.Windows.Forms.TextBox ExamBox1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label SquareLabel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox EdgeBox;
+    }
+}
+
---
+++ 19.GetValueFromTheKeyboard/GetValueFromTheKeyboard/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetValueFromTheKeyboard
+{
+    public partial class BasicApps : Form
+    {
+        public BasicApps()
+        {
+            InitializeComponent();
+        }
+
+        private void WriteButton_Click(object sender, EventArgs e)
+        {
+            //Uygulama11:Klavyeden girilen sayıyı label'a yazdıran programı kodlayınız.
+            //Sayı değerini int olarak tanımladık, textbox'dan alıp int olarak convert ile değiştirdik. Label üzerine string olarak yazdırdık.
+            int sayi;
+            sayi = Convert.ToInt16(ValueTextBox.Text);
+            WriteLabel.Text ="Sayı Değeri: " + sayi.ToString();
+        }
+
+        private void CupeButton_Click(object sender, EventArgs e)
+        {
+            //Uygulama12:Klavyeden girilen sayının küpünü hesaplayan programı yazınız.
+            int kup = 0,sonuc=0;
+            kup = Convert.ToInt32(CupeTextBox.Text);
+            sonuc = kup * kup * kup;
+            ResultLabel.Text = "İşlem Sonucu: " + sonuc.ToString();
+        }
+
+        private void GatherButton_Click(object sender, EventArgs e)
+        {
+            //Uygulama13:Klavyeden girilen iki sayı değeri toplayan programı yazınız.
+            int value1 = 0, value2 = 0, gather = 0;
+            value1 = Convert.ToInt32(Value1TextBox.Text);
+            value2 = Convert.ToInt32(Value2TextBox.Text);
+            gather = value1 + value2;
+            GatherLabel.Text = "Toplam: " + gather.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Uygulama14:Klavyeden bir kenarı girilen karenin alan ve çevresini hesaplayan programı kodlayalım.
+            int kenar = 0, alan=0, cevre=0;
+            kenar = Convert.ToInt32(EdgeBox.Text);
+            alan = kenar * kenar;
+            cevre = kenar * 4;
+            SquareLabel.Text = "Karenin Alanı: " + alan.ToString() + " Karenin Çevresi: " + cevre.ToString();
+        }
+
+        private void AverageCalculateButton_Click(object sender, EventArgs e)
+        {
+            //Uygulama15: Klavyeden 2 dınav notu girilen öğrencinin not ortalamasını hesaplayan programı kodlayınız.
+            int not1 = 0, not2 = 0, ort = 0;
+            not1 = Convert.ToInt32(ExamBox1.Text);
+            not2 = Convert.ToInt32(ExamBox2.Text);
+            ort = (not1 + not2) / 2;
+            AverageCalculateLabel.Text = "Ortalama: " + ort.ToString();
+
+        }
+    }
+}
