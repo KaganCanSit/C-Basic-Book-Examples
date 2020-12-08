00ff4afe809274743f002b74b11b4d1f57ff69a0
kagancansit <kagancansit@hotmail.com>
2020-12-08 16:29:12 +0300

    Basic Char

---
+++ 21.Char/21.Char/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace _21.Char
+{
+    partial class BasicChar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Uygulama20 = new System.Windows.Forms.GroupBox();
+            this.LatterButton = new System.Windows.Forms.Button();
+            this.LetterLabel = new System.Windows.Forms.Label();
+            this.Uygulama22 = new System.Windows.Forms.GroupBox();
+            this.GiveButton = new System.Windows.Forms.Button();
+            this.ExamTextBox3 = new System.Windows.Forms.TextBox();
+            this.ExamTextBox2 = new System.Windows.Forms.TextBox();
+            this.ExamTextBox1 = new System.Windows.Forms.TextBox();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultListBox = new System.Windows.Forms.ListBox();
+            this.Uygulama21 = new System.Windows.Forms.GroupBox();
+            this.GetLatterButton = new System.Windows.Forms.Button();
+            this.WriteLatter = new System.Windows.Forms.Label();
+            this.GetLatterTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Uygulama20.SuspendLayout();
+            this.Uygulama22.SuspendLayout();
+            this.Uygulama21.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Uygulama20
+            // 
+            this.Uygulama20.Controls.Add(this.LatterButton);
+            this.Uygulama20.Controls.Add(this.LetterLabel);
+            this.Uygulama20.Location = new System.Drawing.Point(12, 12);
+            this.Uygulama20.Name = "Uygulama20";
+            this.Uygulama20.Size = new System.Drawing.Size(303, 239);
+            this.Uygulama20.TabIndex = 0;
+            this.Uygulama20.TabStop = false;
+            this.Uygulama20.Text = "Uygulama20";
+            // 
+            // LatterButton
+            // 
+            this.LatterButton.Location = new System.Drawing.Point(89, 117);
+            this.LatterButton.Name = "LatterButton";
+            this.LatterButton.Size = new System.Drawing.Size(117, 37);
+            this.LatterButton.TabIndex = 1;
+            this.LatterButton.Text = "Harfi Yazdır";
+            this.LatterButton.UseVisualStyleBackColor = true;
+            this.LatterButton.Click += new System.EventHandler(this.LatterButton_Click);
+            // 
+            // LetterLabel
+            // 
+            this.LetterLabel.AutoSize = true;
+            this.LetterLabel.Location = new System.Drawing.Point(86, 85);
+            this.LetterLabel.Name = "LetterLabel";
+            this.LetterLabel.Size = new System.Drawing.Size(63, 17);
+            this.LetterLabel.TabIndex = 0;
+            this.LetterLabel.Text = "Harfimiz:";
+            // 
+            // Uygulama22
+            // 
+            this.Uygulama22.Controls.Add(this.SurnameTextBox);
+            this.Uygulama22.Controls.Add(this.NameTextBox);
+            this.Uygulama22.Controls.Add(this.label3);
+            this.Uygulama22.Controls.Add(this.label2);
+            this.Uygulama22.Controls.Add(this.ResultListBox);
+            this.Uygulama22.Location = new System.Drawing.Point(321, 12);
+            this.Uygulama22.Name = "Uygulama22";
+            this.Uygulama22.Size = new System.Drawing.Size(404, 484);
+            this.Uygulama22.TabIndex = 1;
+            this.Uygulama22.TabStop = false;
+            this.Uygulama22.Text = "Uygulama22";
+            // 
+            // GiveButton
+            // 
+            this.GiveButton.Location = new System.Drawing.Point(141, 116);
+            this.GiveButton.Name = "GiveButton";
+            this.GiveButton.Size = new System.Drawing.Size(190, 31);
+            this.GiveButton.TabIndex = 11;
+            this.GiveButton.Text = "Bilgileri Al";
+            this.GiveButton.UseVisualStyleBackColor = true;
+            this.GiveButton.Click += new System.EventHandler(this.GiveButton_Click);
+            // 
+            // ExamTextBox3
+            // 
+            this.ExamTextBox3.Location = new System.Drawing.Point(141, 84);
+            this.ExamTextBox3.Name = "ExamTextBox3";
+            this.ExamTextBox3.Size = new System.Drawing.Size(190, 22);
+            this.ExamTextBox3.TabIndex = 10;
+            // 
+            // ExamTextBox2
+            // 
+            this.ExamTextBox2.Location = new System.Drawing.Point(141, 56);
+            this.ExamTextBox2.Name = "ExamTextBox2";
+            this.ExamTextBox2.Size = new System.Drawing.Size(190, 22);
+            this.ExamTextBox2.TabIndex = 9;
+            // 
+            // ExamTextBox1
+            // 
+            this.ExamTextBox1.Location = new System.Drawing.Point(141, 26);
+            this.ExamTextBox1.Name = "ExamTextBox1";
+            this.ExamTextBox1.Size = new System.Drawing.Size(190, 22);
+            this.ExamTextBox1.TabIndex = 8;
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(118, 56);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(190, 22);
+            this.SurnameTextBox.TabIndex = 7;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(118, 26);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(190, 22);
+            this.NameTextBox.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Sınav3:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sınav2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sınav1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad:";
+            // 
+            // ResultListBox
+            // 
+            this.ResultListBox.FormattingEnabled = true;
+            this.ResultListBox.ItemHeight = 16;
+            this.ResultListBox.Location = new System.Drawing.Point(6, 266);
+            this.ResultListBox.Name = "ResultListBox";
+            this.ResultListBox.Size = new System.Drawing.Size(392, 212);
+            this.ResultListBox.TabIndex = 0;
+            // 
+            // Uygulama21
+            // 
+            this.Uygulama21.Controls.Add(this.GetLatterButton);
+            this.Uygulama21.Controls.Add(this.WriteLatter);
+            this.Uygulama21.Controls.Add(this.GetLatterTextBox);
+            this.Uygulama21.Controls.Add(this.label1);
+            this.Uygulama21.Location = new System.Drawing.Point(12, 257);
+            this.Uygulama21.Name = "Uygulama21";
+            this.Uygulama21.Size = new System.Drawing.Size(303, 239);
+            this.Uygulama21.TabIndex = 1;
+            this.Uygulama21.TabStop = false;
+            this.Uygulama21.Text = "Uygulama21";
+            // 
+            // GetLatterButton
+            // 
+            this.GetLatterButton.Location = new System.Drawing.Point(135, 81);
+            this.GetLatterButton.Name = "GetLatterButton";
+            this.GetLatterButton.Size = new System.Drawing.Size(99, 39);
+            this.GetLatterButton.TabIndex = 3;
+            this.GetLatterButton.Text = "Değeri Al";
+            this.GetLatterButton.UseVisualStyleBackColor = true;
+            this.GetLatterButton.Click += new System.EventHandler(this.GetLatterButton_Click);
+            // 
+            // WriteLatter
+            // 
+            this.WriteLatter.AutoSize = true;
+            this.WriteLatter.Location = new System.Drawing.Point(19, 129);
+            this.WriteLatter.Name = "WriteLatter";
+            this.WriteLatter.Size = new System.Drawing.Size(84, 17);
+            this.WriteLatter.TabIndex = 2;
+            this.WriteLatter.Text = "Girilen Harf:";
+            // 
+            // GetLatterTextBox
+            // 
+            this.GetLatterTextBox.Location = new System.Drawing.Point(105, 44);
+            this.GetLatterTextBox.Name = "GetLatterTextBox";
+            this.GetLatterTextBox.Size = new System.Drawing.Size(174, 22);
+            this.GetLatterTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Harf Giriniz:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.GiveButton);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.ExamTextBox3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.ExamTextBox2);
+            this.groupBox1.Controls.Add(this.ExamTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(330, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 162);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sınavlar";
+            // 
+            // BasicChar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 512);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Uygulama21);
+            this.Controls.Add(this.Uygulama22);
+            this.Controls.Add(this.Uygulama20);
+            this.Name = "BasicChar";
+            this.Text = "Char Örnekleri";
+            this.Uygulama20.ResumeLayout(false);
+            this.Uygulama20.PerformLayout();
+            this.Uygulama22.ResumeLayout(false);
+            this.Uygulama22.PerformLayout();
+            this.Uygulama21.ResumeLayout(false);
+            this.Uygulama21.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Uygulama20;
+        private System.Windows.Forms.GroupBox Uygulama22;
+        private System.Windows.Forms.GroupBox Uygulama21;
+        private System.Windows.Forms.Button LatterButton;
+        private System.Windows.Forms.Label LetterLabel;
+        private System.Windows.Forms.Button GetLatterButton;
+        private System.Windows.Forms.Label WriteLatter;
+        private System.Windows.Forms.TextBox GetLatterTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox ResultListBox;
+        private System.Windows.Forms.Button GiveButton;
+        private System.Windows.Forms.TextBox ExamTextBox3;
+        private System.Windows.Forms.TextBox ExamTextBox2;
+        private System.Windows.Forms.TextBox ExamTextBox1;
+        private System.Windows.Forms.TextBox SurnameTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ 21.Char/21.Char/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21.Char
+{
+    public partial class BasicChar : Form
+    {
+        public BasicChar()
+        {
+            InitializeComponent();
+        }
+
+        //Uygulama20: Kod kısmından girilen char değişkeni labela yazdıran programı kodlayınız.
+        private void LatterButton_Click(object sender, EventArgs e)
+        {
+            char harf='k';
+            LetterLabel.Text ="Harfimiz: " +  harf.ToString();
+        }
+
+
+        //Uygulama21: Klavyeden girilen harfi char değişkeni olarak alıp label'a yazdıran programı kodlayınız.
+        private void GetLatterButton_Click(object sender, EventArgs e)
+        {
+            char harfal;
+            harfal = Convert.ToChar(GetLatterTextBox.Text);
+            WriteLatter.Text ="Girilen Harf: " + harfal.ToString();
+        }
+        /*
+            Uygulama23:Kullanıcı tarafından ad,soyad, sınav1, sınav2, sınav3 bilgileri girilen kişinin 
+            adını ,soyadını ve ortalamasını ListBox'a ekleyen programı yazınız.
+        */
+        private void GiveButton_Click(object sender, EventArgs e)
+        {
+            string ad, soyad;
+            double sinav1, sinav2, sinav3, ortalama;
+
+            ad = NameTextBox.Text;
+            soyad = SurnameTextBox.Text;
+
+            sinav1 = Convert.ToDouble(ExamTextBox1.Text);
+            sinav2 = Convert.ToDouble(ExamTextBox2.Text);
+            sinav3 = Convert.ToDouble(ExamTextBox3.Text);
+            ortalama = (sinav1 + sinav2 + sinav3) / 3;
+
+            ResultListBox.Items.Add(ad + " " + soyad + "- Ortalama: " + ortalama.ToString());
+        }
+    }
+}
