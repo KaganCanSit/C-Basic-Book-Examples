e3f9afd6f9eb48366fc54c09a65780f0eca31735
kagancansit <kagancansit@hotmail.com>
2020-12-11 21:17:12 +0300

    Basic if-else Examples 1

---
+++ 26.DecisionStructures-if-else-for/if-else/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿
+namespace if_else
+{
+    partial class IfElseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.WaterTextBox = new System.Windows.Forms.TextBox();
+            this.HeadLabel = new System.Windows.Forms.Label();
+            this.WaterLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ExamLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ExamTextBox1 = new System.Windows.Forms.TextBox();
+            this.ExamButton = new System.Windows.Forms.Button();
+            this.ExamTextBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.TeamLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TeamTextBox = new System.Windows.Forms.TextBox();
+            this.TeamButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.NumberResultLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.NumberButton = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.TurkeyLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TranslateButton = new System.Windows.Forms.Button();
+            this.EngComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(36, 81);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(146, 30);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.Text = "Durumu Kontrol Et";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.WaterLabel);
+            this.groupBox1.Controls.Add(this.HeadLabel);
+            this.groupBox1.Controls.Add(this.WaterTextBox);
+            this.groupBox1.Controls.Add(this.CheckButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 153);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uygulama6";
+            // 
+            // WaterTextBox
+            // 
+            this.WaterTextBox.Location = new System.Drawing.Point(36, 53);
+            this.WaterTextBox.Name = "WaterTextBox";
+            this.WaterTextBox.Size = new System.Drawing.Size(146, 22);
+            this.WaterTextBox.TabIndex = 1;
+            // 
+            // HeadLabel
+            // 
+            this.HeadLabel.AutoSize = true;
+            this.HeadLabel.Location = new System.Drawing.Point(36, 30);
+            this.HeadLabel.Name = "HeadLabel";
+            this.HeadLabel.Size = new System.Drawing.Size(148, 17);
+            this.HeadLabel.TabIndex = 2;
+            this.HeadLabel.Text = "Suyun Durumu Nedir?";
+            // 
+            // WaterLabel
+            // 
+            this.WaterLabel.AutoSize = true;
+            this.WaterLabel.Location = new System.Drawing.Point(36, 114);
+            this.WaterLabel.Name = "WaterLabel";
+            this.WaterLabel.Size = new System.Drawing.Size(80, 17);
+            this.WaterLabel.TabIndex = 3;
+            this.WaterLabel.Text = "Suyun Hali:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ExamTextBox2);
+            this.groupBox2.Controls.Add(this.ExamLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.ExamTextBox1);
+            this.groupBox2.Controls.Add(this.ExamButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 180);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(214, 200);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Uygulama7";
+            // 
+            // ExamLabel
+            // 
+            this.ExamLabel.AutoSize = true;
+            this.ExamLabel.Location = new System.Drawing.Point(33, 148);
+            this.ExamLabel.Name = "ExamLabel";
+            this.ExamLabel.Size = new System.Drawing.Size(74, 34);
+            this.ExamLabel.TabIndex = 3;
+            this.ExamLabel.Text = "Ortalama: \r\nDurum:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ortalama Ne Kadar?";
+            // 
+            // ExamTextBox1
+            // 
+            this.ExamTextBox1.Location = new System.Drawing.Point(36, 53);
+            this.ExamTextBox1.Name = "ExamTextBox1";
+            this.ExamTextBox1.Size = new System.Drawing.Size(146, 22);
+            this.ExamTextBox1.TabIndex = 1;
+            // 
+            // ExamButton
+            // 
+            this.ExamButton.Location = new System.Drawing.Point(36, 115);
+            this.ExamButton.Name = "ExamButton";
+            this.ExamButton.Size = new System.Drawing.Size(146, 30);
+            this.ExamButton.TabIndex = 0;
+            this.ExamButton.Text = "Notları Al";
+            this.ExamButton.UseVisualStyleBackColor = true;
+            this.ExamButton.Click += new System.EventHandler(this.ExamButton_Click);
+            // 
+            // ExamTextBox2
+            // 
+            this.ExamTextBox2.Location = new System.Drawing.Point(36, 81);
+            this.ExamTextBox2.Name = "ExamTextBox2";
+            this.ExamTextBox2.Size = new System.Drawing.Size(146, 22);
+            this.ExamTextBox2.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.TeamLabel);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.TeamTextBox);
+            this.groupBox3.Controls.Add(this.TeamButton);
+            this.groupBox3.Location = new System.Drawing.Point(232, 21);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(214, 153);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Uygulama7";
+            // 
+            // TeamLabel
+            // 
+            this.TeamLabel.AutoSize = true;
+            this.TeamLabel.Location = new System.Drawing.Point(36, 114);
+            this.TeamLabel.Name = "TeamLabel";
+            this.TeamLabel.Size = new System.Drawing.Size(50, 17);
+            this.TeamLabel.TabIndex = 3;
+            this.TeamLabel.Text = "Takım:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Takımınız Nedir? g-G/f-F/b-B";
+            // 
+            // TeamTextBox
+            // 
+            this.TeamTextBox.Location = new System.Drawing.Point(36, 53);
+            this.TeamTextBox.Name = "TeamTextBox";
+            this.TeamTextBox.Size = new System.Drawing.Size(146, 22);
+            this.TeamTextBox.TabIndex = 1;
+            // 
+            // TeamButton
+            // 
+            this.TeamButton.Location = new System.Drawing.Point(36, 81);
+            this.TeamButton.Name = "TeamButton";
+            this.TeamButton.Size = new System.Drawing.Size(146, 30);
+            this.TeamButton.TabIndex = 0;
+            this.TeamButton.Text = "Takımı Yazdır";
+            this.TeamButton.UseVisualStyleBackColor = true;
+            this.TeamButton.Click += new System.EventHandler(this.TeamButton_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.NumberResultLabel);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.NumberTextBox);
+            this.groupBox4.Controls.Add(this.NumberButton);
+            this.groupBox4.Location = new System.Drawing.Point(232, 180);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(214, 200);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Uygulama7";
+            // 
+            // NumberResultLabel
+            // 
+            this.NumberResultLabel.AutoSize = true;
+            this.NumberResultLabel.Location = new System.Drawing.Point(36, 134);
+            this.NumberResultLabel.Name = "NumberResultLabel";
+            this.NumberResultLabel.Size = new System.Drawing.Size(52, 17);
+            this.NumberResultLabel.TabIndex = 3;
+            this.NumberResultLabel.Text = "Sonuç:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 34);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Hem 2\'ye Hem 3\'e \r\nBölüm Kontrolü";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.Location = new System.Drawing.Point(36, 73);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(146, 22);
+            this.NumberTextBox.TabIndex = 1;
+            // 
+            // NumberButton
+            // 
+            this.NumberButton.Location = new System.Drawing.Point(36, 101);
+            this.NumberButton.Name = "NumberButton";
+            this.NumberButton.Size = new System.Drawing.Size(146, 30);
+            this.NumberButton.TabIndex = 0;
+            this.NumberButton.Text = "Sonucu Hesapla";
+            this.NumberButton.UseVisualStyleBackColor = true;
+            this.NumberButton.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.EngComboBox);
+            this.groupBox5.Controls.Add(this.TurkeyLabel);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.TranslateButton);
+            this.groupBox5.Location = new System.Drawing.Point(452, 102);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(214, 181);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Uygulama8";
+            // 
+            // TurkeyLabel
+            // 
+            this.TurkeyLabel.AutoSize = true;
+            this.TurkeyLabel.Location = new System.Drawing.Point(21, 99);
+            this.TurkeyLabel.Name = "TurkeyLabel";
+            this.TurkeyLabel.Size = new System.Drawing.Size(56, 17);
+            this.TurkeyLabel.TabIndex = 3;
+            this.TurkeyLabel.Text = "Türkçe:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "İngilizce - Türkçe Kelime";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TranslateButton
+            // 
+            this.TranslateButton.Location = new System.Drawing.Point(32, 126);
+            this.TranslateButton.Name = "TranslateButton";
+            this.TranslateButton.Size = new System.Drawing.Size(146, 30);
+            this.TranslateButton.TabIndex = 0;
+            this.TranslateButton.Text = "Çevir";
+            this.TranslateButton.UseVisualStyleBackColor = true;
+            this.TranslateButton.Click += new System.EventHandler(this.TranslateButton_Click);
+            // 
+            // EngComboBox
+            // 
+            this.EngComboBox.FormattingEnabled = true;
+            this.EngComboBox.Items.AddRange(new object[] {
+            "Apple",
+            "Melon",
+            "Orange",
+            "Banana",
+            "Peach"});
+            this.EngComboBox.Location = new System.Drawing.Point(24, 66);
+            this.EngComboBox.Name = "EngComboBox";
+            this.EngComboBox.Size = new System.Drawing.Size(171, 24);
+            this.EngComboBox.TabIndex = 4;
+            // 
+            // IfElseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 395);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "IfElseForm";
+            this.Text = "if else Examples";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label WaterLabel;
+        private System.Windows.Forms.Label HeadLabel;
+        private System.Windows.Forms.TextBox WaterTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label ExamLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ExamTextBox1;
+        private System.Windows.Forms.Button ExamButton;
+        private System.Windows.Forms.TextBox ExamTextBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label TeamLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TeamTextBox;
+        private System.Windows.Forms.Button TeamButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label NumberResultLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.Button NumberButton;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ComboBox EngComboBox;
+        private System.Windows.Forms.Label TurkeyLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button TranslateButton;
+    }
+}
+
---
+++ 26.DecisionStructures-if-else-for/if-else/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_else
+{
+    public partial class IfElseForm : Form
+    {
+        public IfElseForm()
+        {
+            InitializeComponent();
+        }
+
+
+        /*
+         * Uygulama6:
+           Klavyeden girilen değere göre suyun durumunu(su<0 - Katı 100>su>0 - Sıvı Su>100 - Gaz) yazdıran prg. yazınız.
+        */
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            int su;
+            su = Convert.ToInt32(WaterTextBox.Text);
+
+            if(su<100 || su>0)
+            {
+                WaterLabel.Text = "Suyun Hali: Sıvıdır";
+            }
+            if(su<=0)
+            {
+                WaterLabel.Text = "Suyun Hali: Katıdır";
+            }
+            if(su>=100)
+            {
+                WaterLabel.Text = "Suyun Hali: Gazdır";
+            }
+        }
+
+
+        /*
+         *Uygulama7: Klavyeden 2 sınav notu girilen öğrencinin ortalamasına göre başarı durumunu yazdıran prg. kodlayınız. 
+         *
+         *Durumlar;
+         *0-40 arası Vasat
+         *41-60 arası Orta
+         *61-75 arası İyi
+         *76-100 arası Çok İyi
+        */
+        private void ExamButton_Click(object sender, EventArgs e)
+        {
+            int exam1, exam2, average;
+
+            exam1 = Convert.ToInt32(ExamTextBox1.Text);
+            exam2 = Convert.ToInt32(ExamTextBox2.Text);
+            average = (exam1 + exam2) / 2;
+            
+            //Sınav değerleri için 0/100 kontrolü
+            if(exam1<0 || exam1>100 || exam2 < 0 || exam2 > 100)
+            {
+                MessageBox.Show("Sınav Notu Değerleri 1-100 Arasında Olmaladır.");
+                Environment.Exit(0); //Bu aralıkta not girişi varsa programı sonlandır.
+            }
+
+
+            //Notlara göre ortalama ve durum sonuçları
+            if (average>0 && average<40)
+            {
+                ExamLabel.Text = "Ortalama: " + average + Environment.NewLine + "Durum: Vasat";
+            }
+            else if(average>41 && average<60)
+            {
+                ExamLabel.Text = "Ortalama: " + average + Environment.NewLine + "Durum: Orta";
+            }
+            else if (average > 61 && average < 75)
+            {
+                ExamLabel.Text = "Ortalama: " + average + Environment.NewLine + "Durum: İyi";
+            }
+            else if (average > 76 && average < 100)
+            {
+                ExamLabel.Text = "Ortalama: " + average + Environment.NewLine + "Durum: Çok İyi";
+            }
+        }
+
+
+        //Uygulama8: Klavyeden girilen harfe göre takımı yazdıran prg. yazınız.(g-G=Galatasaray /f-F=Fenerbahçe // b-B=Beşiktaş)
+        private void TeamButton_Click(object sender, EventArgs e)
+        {
+            if(TeamTextBox.Text=="g" || TeamTextBox.Text=="G")
+            {
+                TeamLabel.Text = "Takım: Galatasaray";
+            }
+            else if (TeamTextBox.Text == "f" || TeamTextBox.Text == "F")
+            {
+                TeamLabel.Text = "Takım: Fenerbahçe";
+            }
+            else if (TeamTextBox.Text == "b" || TeamTextBox.Text == "B")
+            {
+                TeamLabel.Text = "Takım: Beşiktaş";
+            }
+            else
+            {
+                MessageBox.Show("Tanımlanmayan Karakter Girdiniz. Yeniden Deneyiniz.");
+                Environment.Exit(0);
+            }
+        }
+
+
+        //Uygulama9: Klavyeden girilen sayının 2'ye ve 3'e tam bölünüp bölünmediğini kontrolünü yapan prog. yazınız.
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            int Number;
+            Number = Convert.ToInt32(NumberTextBox.Text);
+
+            if(Number%2==0 && Number%3==0)
+            {
+                NumberResultLabel.Text = "Sonuç:" + Environment.NewLine + "Sayı Değeri Bölünebilir";
+            }
+            else
+            {
+                NumberResultLabel.Text = "Sonuç:" + Environment.NewLine + "Sayı Değeri Bölünemez";
+            }
+
+        }
+
+        /*
+         * Uygulama10: 5 kelimeden oluşan bir İngilizce-Türkçe kelime bloğu oluşturalım. Kelimelerin İnglizcelerini ComboBox
+         * aracına ekleyelim. ComboBox'tan seçtiğimiz kelimeleye göre Türkçe karşılığını Label'a yazdıralım.
+        */
+        private void TranslateButton_Click(object sender, EventArgs e)
+        {
+            //İlk değerlerini ComboBox aracına form yüzünden Edit Items diyerek alt alta ekledik.
+            if(EngComboBox.Text=="Apple")
+            {
+                TurkeyLabel.Text = "Türkçe: Elma";
+            }
+            else if(EngComboBox.Text == "Melon")
+            {
+                TurkeyLabel.Text = "Türkçe: Kavun";
+            }
+            else if(EngComboBox.Text == "Orange")
+            {
+                TurkeyLabel.Text = "Türkçe: Portakal";
+            }
+            else if(EngComboBox.Text == "Banan")
+            {
+                TurkeyLabel.Text = "Türkçe: Muz";
+            }
+            else if(EngComboBox.Text == "Peach")
+            {
+                TurkeyLabel.Text = "Türkçe: Şeftali";
+            }
+        }
+    }
+}
