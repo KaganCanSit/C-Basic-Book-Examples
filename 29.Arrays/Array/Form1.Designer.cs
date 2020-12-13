12508f82078db49f586d5dcab896c73426e985d8
kagancansit <kagancansit@hotmail.com>
2020-12-13 20:57:34 +0300

    Array Basic

---
+++ 29.Arrays/Array/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿
+namespace Array
+{
+    partial class Array
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.ColorLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.MarkaButton = new System.Windows.Forms.Button();
+            this.MarkaLabel = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SayıButton = new System.Windows.Forms.Button();
+            this.SayıLabel = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.FirmaButton = new System.Windows.Forms.Button();
+            this.FirmalarListBox = new System.Windows.Forms.ListBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.OndalikliListBox = new System.Windows.Forms.ListBox();
+            this.OndalikliButton = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.SehirListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SehirTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox7 = new System.Windows.Forms.GroupBox();
+            this.EnBuyukButton = new System.Windows.Forms.Button();
+            this.EnBuyukLabel = new System.Windows.Forms.Label();
+            this.groupBox8 = new System.Windows.Forms.GroupBox();
+            this.EnKucukButton = new System.Windows.Forms.Button();
+            this.EnKucukLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            this.groupBox7.SuspendLayout();
+            this.groupBox8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(46, 36);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(103, 27);
+            this.ColorButton.TabIndex = 0;
+            this.ColorButton.Text = "Rengi Yazdır";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // ColorLabel
+            // 
+            this.ColorLabel.AutoSize = true;
+            this.ColorLabel.Location = new System.Drawing.Point(46, 66);
+            this.ColorLabel.Name = "ColorLabel";
+            this.ColorLabel.Size = new System.Drawing.Size(20, 17);
+            this.ColorLabel.TabIndex = 1;
+            this.ColorLabel.Text = "...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ColorButton);
+            this.groupBox1.Controls.Add(this.ColorLabel);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 104);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uygulama1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.MarkaButton);
+            this.groupBox2.Controls.Add(this.MarkaLabel);
+            this.groupBox2.Location = new System.Drawing.Point(12, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 104);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Uygulama2";
+            // 
+            // MarkaButton
+            // 
+            this.MarkaButton.Location = new System.Drawing.Point(46, 36);
+            this.MarkaButton.Name = "MarkaButton";
+            this.MarkaButton.Size = new System.Drawing.Size(113, 27);
+            this.MarkaButton.TabIndex = 0;
+            this.MarkaButton.Text = "Markayı Yazdır";
+            this.MarkaButton.UseVisualStyleBackColor = true;
+            this.MarkaButton.Click += new System.EventHandler(this.MarkaButton_Click);
+            // 
+            // MarkaLabel
+            // 
+            this.MarkaLabel.AutoSize = true;
+            this.MarkaLabel.Location = new System.Drawing.Point(46, 66);
+            this.MarkaLabel.Name = "MarkaLabel";
+            this.MarkaLabel.Size = new System.Drawing.Size(20, 17);
+            this.MarkaLabel.TabIndex = 1;
+            this.MarkaLabel.Text = "...";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.SayıButton);
+            this.groupBox3.Controls.Add(this.SayıLabel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 232);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 104);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Uygulama3";
+            // 
+            // SayıButton
+            // 
+            this.SayıButton.Location = new System.Drawing.Point(46, 36);
+            this.SayıButton.Name = "SayıButton";
+            this.SayıButton.Size = new System.Drawing.Size(113, 27);
+            this.SayıButton.TabIndex = 0;
+            this.SayıButton.Text = "Sayıyı Yazdır";
+            this.SayıButton.UseVisualStyleBackColor = true;
+            this.SayıButton.Click += new System.EventHandler(this.SayıButton_Click);
+            // 
+            // SayıLabel
+            // 
+            this.SayıLabel.AutoSize = true;
+            this.SayıLabel.Location = new System.Drawing.Point(46, 66);
+            this.SayıLabel.Name = "SayıLabel";
+            this.SayıLabel.Size = new System.Drawing.Size(20, 17);
+            this.SayıLabel.TabIndex = 1;
+            this.SayıLabel.Text = "...";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.FirmalarListBox);
+            this.groupBox4.Controls.Add(this.FirmaButton);
+            this.groupBox4.Location = new System.Drawing.Point(218, 197);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 179);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Uygulama4";
+            // 
+            // FirmaButton
+            // 
+            this.FirmaButton.Location = new System.Drawing.Point(43, 28);
+            this.FirmaButton.Name = "FirmaButton";
+            this.FirmaButton.Size = new System.Drawing.Size(116, 27);
+            this.FirmaButton.TabIndex = 0;
+            this.FirmaButton.Text = "Firmaları Yazdır";
+            this.FirmaButton.UseVisualStyleBackColor = true;
+            this.FirmaButton.Click += new System.EventHandler(this.FirmaButton_Click);
+            // 
+            // FirmalarListBox
+            // 
+            this.FirmalarListBox.FormattingEnabled = true;
+            this.FirmalarListBox.ItemHeight = 16;
+            this.FirmalarListBox.Location = new System.Drawing.Point(6, 70);
+            this.FirmalarListBox.Name = "FirmalarListBox";
+            this.FirmalarListBox.Size = new System.Drawing.Size(180, 100);
+            this.FirmalarListBox.TabIndex = 2;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.OndalikliListBox);
+            this.groupBox5.Controls.Add(this.OndalikliButton);
+            this.groupBox5.Location = new System.Drawing.Point(218, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 179);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Uygulama5";
+            // 
+            // OndalikliListBox
+            // 
+            this.OndalikliListBox.FormattingEnabled = true;
+            this.OndalikliListBox.ItemHeight = 16;
+            this.OndalikliListBox.Location = new System.Drawing.Point(6, 70);
+            this.OndalikliListBox.Name = "OndalikliListBox";
+            this.OndalikliListBox.Size = new System.Drawing.Size(180, 100);
+            this.OndalikliListBox.TabIndex = 2;
+            // 
+            // OndalikliButton
+            // 
+            this.OndalikliButton.Location = new System.Drawing.Point(6, 28);
+            this.OndalikliButton.Name = "OndalikliButton";
+            this.OndalikliButton.Size = new System.Drawing.Size(180, 27);
+            this.OndalikliButton.TabIndex = 0;
+            this.OndalikliButton.Text = "Ondalıklı Sayıları Yazdır";
+            this.OndalikliButton.UseVisualStyleBackColor = true;
+            this.OndalikliButton.Click += new System.EventHandler(this.OndalikliButton_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.button2);
+            this.groupBox6.Controls.Add(this.label1);
+            this.groupBox6.Controls.Add(this.SehirTextBox);
+            this.groupBox6.Controls.Add(this.SehirListBox);
+            this.groupBox6.Controls.Add(this.button1);
+            this.groupBox6.Location = new System.Drawing.Point(424, 12);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(200, 324);
+            this.groupBox6.TabIndex = 7;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Uygulama6";
+            // 
+            // SehirListBox
+            // 
+            this.SehirListBox.FormattingEnabled = true;
+            this.SehirListBox.ItemHeight = 16;
+            this.SehirListBox.Location = new System.Drawing.Point(6, 175);
+            this.SehirListBox.Name = "SehirListBox";
+            this.SehirListBox.Size = new System.Drawing.Size(180, 132);
+            this.SehirListBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Şehirleri Yazdır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SehirTextBox
+            // 
+            this.SehirTextBox.Location = new System.Drawing.Point(23, 53);
+            this.SehirTextBox.Name = "SehirTextBox";
+            this.SehirTextBox.Size = new System.Drawing.Size(146, 22);
+            this.SehirTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Şehri Yazınız";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 38);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "5\'lik diziye ekle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox7
+            // 
+            this.groupBox7.Controls.Add(this.EnBuyukButton);
+            this.groupBox7.Controls.Add(this.EnBuyukLabel);
+            this.groupBox7.Location = new System.Drawing.Point(630, 12);
+            this.groupBox7.Name = "groupBox7";
+            this.groupBox7.Size = new System.Drawing.Size(200, 104);
+            this.groupBox7.TabIndex = 5;
+            this.groupBox7.TabStop = false;
+            this.groupBox7.Text = "Uygulama7";
+            // 
+            // EnBuyukButton
+            // 
+            this.EnBuyukButton.Location = new System.Drawing.Point(46, 36);
+            this.EnBuyukButton.Name = "EnBuyukButton";
+            this.EnBuyukButton.Size = new System.Drawing.Size(113, 27);
+            this.EnBuyukButton.TabIndex = 0;
+            this.EnBuyukButton.Text = "En Büyük Sayı";
+            this.EnBuyukButton.UseVisualStyleBackColor = true;
+            this.EnBuyukButton.Click += new System.EventHandler(this.EnBuyukButton_Click);
+            // 
+            // EnBuyukLabel
+            // 
+            this.EnBuyukLabel.AutoSize = true;
+            this.EnBuyukLabel.Location = new System.Drawing.Point(46, 66);
+            this.EnBuyukLabel.Name = "EnBuyukLabel";
+            this.EnBuyukLabel.Size = new System.Drawing.Size(20, 17);
+            this.EnBuyukLabel.TabIndex = 1;
+            this.EnBuyukLabel.Text = "...";
+            // 
+            // groupBox8
+            // 
+            this.groupBox8.Controls.Add(this.EnKucukButton);
+            this.groupBox8.Controls.Add(this.EnKucukLabel);
+            this.groupBox8.Location = new System.Drawing.Point(630, 122);
+            this.groupBox8.Name = "groupBox8";
+            this.groupBox8.Size = new System.Drawing.Size(200, 104);
+            this.groupBox8.TabIndex = 6;
+            this.groupBox8.TabStop = false;
+            this.groupBox8.Text = "Uygulama8";
+            // 
+            // EnKucukButton
+            // 
+            this.EnKucukButton.Location = new System.Drawing.Point(46, 36);
+            this.EnKucukButton.Name = "EnKucukButton";
+            this.EnKucukButton.Size = new System.Drawing.Size(113, 27);
+            this.EnKucukButton.TabIndex = 0;
+            this.EnKucukButton.Text = "En Küçük Sayı";
+            this.EnKucukButton.UseVisualStyleBackColor = true;
+            this.EnKucukButton.Click += new System.EventHandler(this.EnKucukButton_Click);
+            // 
+            // EnKucukLabel
+            // 
+            this.EnKucukLabel.AutoSize = true;
+            this.EnKucukLabel.Location = new System.Drawing.Point(46, 66);
+            this.EnKucukLabel.Name = "EnKucukLabel";
+            this.EnKucukLabel.Size = new System.Drawing.Size(20, 17);
+            this.EnKucukLabel.TabIndex = 1;
+            this.EnKucukLabel.Text = "...";
+            // 
+            // Array
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 385);
+            this.Controls.Add(this.groupBox8);
+            this.Controls.Add(this.groupBox7);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Array";
+            this.Text = "Diziler";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
+            this.groupBox7.ResumeLayout(false);
+            this.groupBox7.PerformLayout();
+            this.groupBox8.ResumeLayout(false);
+            this.groupBox8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Label ColorLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button MarkaButton;
+        private System.Windows.Forms.Label MarkaLabel;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button SayıButton;
+        private System.Windows.Forms.Label SayıLabel;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button FirmaButton;
+        private System.Windows.Forms.ListBox FirmalarListBox;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox OndalikliListBox;
+        private System.Windows.Forms.Button OndalikliButton;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SehirTextBox;
+        private System.Windows.Forms.ListBox SehirListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox7;
+        private System.Windows.Forms.Button EnBuyukButton;
+        private System.Windows.Forms.Label EnBuyukLabel;
+        private System.Windows.Forms.GroupBox groupBox8;
+        private System.Windows.Forms.Button EnKucukButton;
+        private System.Windows.Forms.Label EnKucukLabel;
+    }
+}
+
---
+++ 29.Arrays/Array/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array
+{
+    public partial class Array : Form
+    {
+        public Array()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * !Dizi aynı zamanda bir sınıftır. Sınıfa dair bir örnek verecek olursak; apartmanı bir sınıf olarak deüşünelim. Apartmanın
+         * içerisinde daireler birer nesne, dairenin iç özellikleri renk,kat,fiyat vs. bunlarda bizim özelliklerimizdir.
+         * Sınıflardan türetilen nesnelerin birbir aynı olması söz konusu değil. Örneğin 1.kattaki biri en küçük odayı çocuk odası 
+         * yaparken, diğer katlar farklı şekillerde özelleştirebilir. Yani farklı özellik ve nitelikler ekleyebilirler. 
+         *
+         * index: Dizinin bellekte kaçıncı sırada tutalacağını belirler.
+         * 
+         * Sınıf Adı NesneAdı = new SınıfAdı
+         * Değiken türü [] Dizi Adı = new Değişken Türü [Eleman Sayısı]
+         */
+
+
+
+
+        /*
+         * Uygulama1: 5 elemandan oluşan bir renkler dizisi tanımlayıp bu diziye veri girişi yapalım ve dizinin 2.index'inde
+         * bulunan elamanı yazdıralım.
+        */
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            string[] renkler = new string[6];
+            renkler[0] = "Sarı";
+            renkler[1] = "Mavi";
+            renkler[2] = "Beyaz";
+            renkler[3] = "Turuncu";
+            renkler[4] = "Kırmızı";
+            ColorLabel.Text = renkler[2];
+        }
+ 
+        //Uygulama2: 4 elamandan oluşan bir iller dizisi oluşturup 1.indexte ki değeri çağıralım.
+        private void MarkaButton_Click(object sender, EventArgs e)
+        {
+            string[] marka = new string[4];
+            marka[0] = "Monster";
+            marka[1] = "Asus";
+            marka[2] = "Lenovo";
+            marka[3] = "Dell";
+
+            MarkaLabel.Text = marka[1];
+        }
+
+        //Uygulama3: 8 elemandan oluşan bir çift sayılar dizisi oluşturup 6.indexinde ki değeri yazdıralım.
+        private void SayıButton_Click(object sender, EventArgs e)
+        {
+            int[] cift = new int[8];
+            cift[0] = 2;
+            cift[1] = 222;
+            cift[2] = 32;
+            cift[3] = 48;
+            cift[4] = 76;
+            cift[5] = 84;
+            cift[6] = 96;
+            cift[7] = 16;
+
+            SayıLabel.Text = Convert.ToString(cift[6]);
+        }
+
+        //Uygulama4: Oluşturulan 5 elemanlı firmalar dizisinin tüm elemanlarını ListBox aracına yazdıran prg. kodlayınız.
+        private void FirmaButton_Click(object sender, EventArgs e)
+        {
+            string[] firma = { "Microsoft", "IBM", "Apple", "Google", "Amazon" };
+
+            for(int i=0;i<5;i++)
+            {
+                FirmalarListBox.Items.Add(firma[i]);
+            }
+        }
+
+        //Uygulama5: 5 elemanlı bir ondalıklı sayı dizisindeki elemanları ListBox aracına yazdıralım.
+        private void OndalikliButton_Click(object sender, EventArgs e)
+        {
+            double[] ondalikli = { 12.15, 123.78, 147.78, 365.1, 11.12 };
+            for (int i = 0; i < 5; i++)
+            {
+                OndalikliListBox.Items.Add(ondalikli[i]);
+            }
+        }
+
+        //!!Önemli Örnek: Uygulama6: Kullanıcıdan 5 adet şehir ismi alıp, ListBox'a ekleyiniz.
+        string[] sehir = new string[5];
+        int sayac = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sehir[sayac] = SehirTextBox.Text;
+            sayac++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<5;i++)
+            {
+                SehirListBox.Items.Add(sehir[i]);
+            }
+        }
+
+        //Uygulama7: Oluşturulan bir dizideki en büyük elemanı bulan programı yazdıralım.
+        private void EnBuyukButton_Click(object sender, EventArgs e)
+        {
+            int[] dizi = {102,45,78,96,74,852,741,964,789,146 };
+            int buyuksayi=0;
+
+            for(int i=0; i<dizi.Length; i++) // Length: Dizi büyüklüğünü bulan program.
+            {
+                if(buyuksayi<dizi[i])
+                {
+                    buyuksayi = dizi[i];
+                }
+            }
+
+            EnBuyukLabel.Text = Convert.ToString(buyuksayi);
+        }
+
+        //Uygulama8: Oluşturulan bir dizideki en küçük elemanı bulan prg. yazınız.
+        private void EnKucukButton_Click(object sender, EventArgs e)
+        {
+            int[] dizi = { -125, 5, 11, 147, -784, 85, 4, 94, 89, 14 };
+            int kucuksayi = 0;
+
+            for (int i = 0; i < dizi.Length; i++) // Length: Dizi büyüklüğünü bulan program.
+            {
+                if (kucuksayi > dizi[i])
+                {
+                    kucuksayi = dizi[i];
+                }
+            }
+
+            EnKucukLabel.Text = Convert.ToString(kucuksayi);
+        }
+    }
+}
